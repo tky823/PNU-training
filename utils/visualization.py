1e10b424cc94cdeb48f87ad33e290636462eb8cd
tky823 <tky823.main+github@gmail.com>
2024-09-22 23:51:00 +0900

    [src] Implement plot_mesh

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,3 +1,4 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import torch
 
@@ -63,6 +64,81 @@ def plot_distribution(
         marker="^",
         edgecolors="blue",
     )
+    fig.savefig(path, bbox_inches="tight")
+
+    plt.close()
+
+
+def plot_mesh(
+    x: torch.Tensor,
+    y: torch.Tensor,
+    z: torch.Tensor,
+    labeled_dataset: DummyPositiveNegativeDataset | None = None,
+    unlabeled_dataset: DummyPositiveNegativeDataset | None = None,
+    path: str | None = None,
+) -> None:
+    if labeled_dataset is None:
+        labeled_positive_inputs = None
+        labeled_negative_inputs = None
+    else:
+        labeled_positive_inputs = []
+        labeled_negative_inputs = []
+
+        for labeled_input, labeled_target in labeled_dataset:
+            if labeled_target.item() == 1:
+                labeled_positive_inputs.append(labeled_input)
+            else:
+                labeled_negative_inputs.append(labeled_input)
+
+        labeled_positive_inputs = torch.stack(labeled_positive_inputs, dim=0)
+        labeled_negative_inputs = torch.stack(labeled_negative_inputs, dim=0)
+
+    if unlabeled_dataset is None:
+        unlabeled_inputs = None
+    else:
+        unlabeled_inputs = []
+
+        for unlabeled_input, _ in unlabeled_dataset:
+            unlabeled_inputs.append(unlabeled_input)
+
+        unlabeled_inputs = torch.stack(unlabeled_inputs, dim=0)
+
+    fig, axis = plt.subplots(figsize=(8, 8))
+
+    norm = mpl.colors.Normalize(vmin=-1, vmax=1)
+    axis.pcolormesh(x, y, z, alpha=0.7, norm=norm, cmap="bwr")
+
+    if unlabeled_inputs is not None:
+        # plot unlabeld inputs at first
+        axis.scatter(
+            unlabeled_inputs[:, 0],
+            unlabeled_inputs[:, 1],
+            s=50,
+            alpha=0.5,
+            color="black",
+            marker="s",
+            edgecolors="black",
+        )
+
+    axis.scatter(
+        labeled_positive_inputs[:, 0],
+        labeled_positive_inputs[:, 1],
+        s=50,
+        alpha=0.5,
+        color="red",
+        marker="o",
+        edgecolors="red",
+    )
+    axis.scatter(
+        labeled_negative_inputs[:, 0],
+        labeled_negative_inputs[:, 1],
+        s=50,
+        alpha=0.5,
+        color="blue",
+        marker="^",
+        edgecolors="blue",
+    )
+
     fig.savefig(path, bbox_inches="tight")
 
     plt.close()
